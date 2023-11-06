ee584b0ce0fafd30a0e48c08c494ab5ddb91ef14
Illia Kovalov <kovalev.illia2009@gmail.com>
2023-11-06 12:40:37 +0100

    272727 final

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,13 @@
 import { Profile } from "../components/Profile/Profile";
-import { Statistics } from '../components/Statistics/Statistics';
+import { Statistics } from "../components/Statistics/Statistics";
 import { FriendList } from "../components/FriendList/FriendList";
+import { TransactionHistory } from "../components/TransactionHistory/TransactionHistory";
+
 
 import user from "../json/user.json";
-import data from '../json/data.json';
-import friends from '../json/friends.json';
+import data from "../json/data.json";
+import friends from "../json/friends.json";
+import transactions from "../json/transactions.json";
 
 export const App = () => {
   return (
@@ -21,6 +24,8 @@ export const App = () => {
       <Statistics stats={data} />
 
       <FriendList friends={friends} />
+
+      <TransactionHistory items={transactions} />;
     </>
   );
 };
